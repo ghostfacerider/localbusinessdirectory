afa062118401aa6060d0d54c398214c6290b070d
alih-ngtsol <alih.ext@ngtsol.com>
2025-01-23 20:29:32 +0500

    testing 3

--- src/components/Card copy 2.tsx
+++ src/components/Card copy 2.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { motion } from "framer-motion"
-import { Link } from "react-router-dom";
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import * as businessTypes from '../config/business-types';
 import * as BusinessService from '../services/BusinessService';
 
