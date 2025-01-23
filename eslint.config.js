16274bf3034dd1dba01af4be9de4ca9598c525a6
alih-ngtsol <alih.ext@ngtsol.com>
2025-01-23 21:06:54 +0500

    testing 8

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const compat = new FlatCompat();
-
-module.exports = [
-  ...compat.config({
-    files: ['src/**/*.{ts/tsx}'],
-    extends: ['./.eslintrc.cjs'],
-  }),
-];
---
+++ eslint.config.ts
@@ -0,0 +1,58 @@
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import jestPlugin from 'eslint-plugin-jest';
+
+export default [
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ignores: [
+      'node_modules/',
+      'build/',
+      'dist/',
+      'package-lock.json',
+    ],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.app.json',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      prettier: prettierPlugin,
+      '@typescript-eslint': tsPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['vite.config.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.vite.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+];
+
