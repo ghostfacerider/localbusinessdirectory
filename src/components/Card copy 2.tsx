cd61f2c47bed424105aa17291157b64602634828
alih-ngtsol <alih.ext@ngtsol.com>
2025-01-23 19:58:27 +0500

    test: pr commit 12

--- src/components/Card copy 2.tsx
+++ src/components/Card copy 2.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion"
 import { Link } from "react-router-dom";
 import * as businessTypes from '../config/business-types';
 import * as BusinessService from '../services/BusinessService';
